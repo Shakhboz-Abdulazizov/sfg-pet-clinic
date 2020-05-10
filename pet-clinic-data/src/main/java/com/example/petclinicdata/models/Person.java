ccb424b5206479013f49b5f53e1c6b2a01c878d7
shakhboz-abdulazizov <shahboz.abdulazizov@mail.ru>
2020-05-11 04:11:21 +0500

    Owners to JPA Entities

--- pet-clinic-data/src/main/java/com/example/petclinicdata/models/Person.java
+++ pet-clinic-data/src/main/java/com/example/petclinicdata/models/Person.java
@@ -1,8 +1,16 @@
 package com.example.petclinicdata.models;
 
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity{
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
